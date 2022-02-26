c288870777c66996df58d93c4b856e9ec04fd15d
Fidel Lim <lim.fidel@gmail.com>
2022-02-27 01:17:30 +0400

    add pointer prototype

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 00:33:48 by flim              #+#    #+#             */
-/*   Updated: 2022/02/26 22:24:58 by flim             ###   ########.fr       */
+/*   Updated: 2022/02/27 00:21:57 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 # include "../libft/libft.h"
 
 int	ft_printf(const char *s, ...);
-int	print_conversion(va_list args, const char c);
 int	print_char(va_list args);
 int	print_str(va_list args);
 int	print_int(va_list args);
 int	print_uint(va_list args);
 int	print_hex(va_list args, char c);
+int	print_pointer(va_list args);
 
 #endif
