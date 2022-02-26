6c074afa807a20d6634013a4a5fdc51cb5c7068a
Fidel Lim <lim.fidel@gmail.com>
2022-02-26 18:30:16 +0400

    update makefile of libft

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 17:52:49 by flim              #+#    #+#             */
+/*   Updated: 2022/01/21 12:20:11 by flim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*ptr;
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	ptr = *lst;
+	while (ptr)
+	{
+		tmp = ptr->next;
+		ft_lstdelone(ptr, del);
+		ptr = tmp;
+	}
+	*lst = NULL;
+}
---
+++ libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 17:44:42 by flim              #+#    #+#             */
+/*   Updated: 2022/01/17 19:23:52 by flim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*tmp;
+
+	tmp = (t_list *) malloc(sizeof(t_list));
+	if (!tmp)
+		return (0);
+	tmp->content = content;
+	tmp->next = NULL;
+	return (tmp);
+}
